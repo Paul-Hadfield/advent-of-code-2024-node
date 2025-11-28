de58ecc9f3e934270202a7c9415cb917759fb28d
Paul-Hadfield <732223+Paul-Hadfield@users.noreply.github.com>
2025-11-28 19:19:28 +0000

    Added execution time

--- 01/2/program.ts
+++ 01/2/program.ts
@@ -1,5 +1,8 @@
 import * as fs from "fs";
 
+import { performance } from "perf_hooks";
+const start = performance.now();
+
 function getCounts(val1: string, vals2: string[]): number {
     let count = 0;
 
@@ -14,8 +17,6 @@ const data2 = data.reduce((acc: Array<Array<string>>, val: string) => {
     acc.push(val.replace(/\s\s+/g, " ").split(" "));
     return acc;
 }, new Array<Array<string>>());
-
-console.log(data2);
 
 const vals1 = data2.map((data) => {
     return data[0];
@@ -33,8 +34,9 @@ const len = vals1.length;
 var result = 0;
 for (let i = 0; i < len; i++) {
     const count = getCounts(vals1[i], vals2);
-    console.log(vals1[i], count);
     result = result + parseInt(vals1[i]) * count;
 }
 
 console.log(result);
+const durationMs = performance.now() - start;
+console.log(`Execution time: ${durationMs.toFixed(3)}ms`);
