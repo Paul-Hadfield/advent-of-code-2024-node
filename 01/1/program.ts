7f57bbb1fe124ee3043e79663a404d40433a9efc
Paul-Hadfield <732223+Paul-Hadfield@users.noreply.github.com>
2025-11-28 19:18:01 +0000

    added timing

--- 01/1/program.ts
+++ 01/1/program.ts
@@ -1,12 +1,13 @@
 import * as fs from "fs";
+import { performance } from "perf_hooks";
+
+const start = performance.now();
 
 const data = fs.readFileSync("./example.txt").toString().split("\n");
 const data2 = data.reduce((acc: Array<Array<string>>, val: string) => {
     acc.push(val.replace(/\s\s+/g, " ").split(" "));
     return acc;
 }, new Array<Array<string>>());
-
-console.log(data2);
 
 const vals1 = data2.map((data) => {
     return data[0];
@@ -19,14 +20,14 @@ const vals2 = data2.map((data) => {
 vals1.sort();
 vals2.sort();
 
-console.log(vals1, vals2);
-
 const len = vals1.length;
 
 var result = 0;
 for (let i = 0; i < len; i++) {
-    console.log(vals1[i], vals2[i]);
     result = result + Math.abs(parseInt(vals1[i]) - parseInt(vals2[i]));
 }
 
 console.log(result);
+
+const durationMs = performance.now() - start;
+console.log(`Execution time: ${durationMs.toFixed(3)}ms`);
